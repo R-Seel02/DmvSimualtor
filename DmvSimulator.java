0d2e51d3acedaa41945db1b5f82cf2f8e949e721
R-Seel02 <Ryanjseely@icloud.com>
2024-03-05 17:50:45 -0500

    fixed error

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -7,7 +7,7 @@ public class DmvSimulator {
         Random random = new Random();
         int userNumber = random.nextInt(100) + 1;
 
-        System.out.println("Your number is " + userNumber + "Sir stop yelling and wait in line");
+        System.out.println("Your number is " + userNumber + " Sir stop yelling and wait in line");
 
         for (int i = userNumber + 1; i <= 100; i++) {
             System.out.println("Calling number " + i);
@@ -16,7 +16,7 @@ public class DmvSimulator {
         }
 
         System.out.println("Calling number " + userNumber);
-        System.out.println("Sorry, you don't have the required paperwork, please come again!");
+        System.out.println("Sorry, you don't have the required paperwork, please come again! ");
 
     }
 }
